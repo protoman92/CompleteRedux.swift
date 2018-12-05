0033b20b505c7c0409ccb7a2eda614e8df7d308a
Hai Pham <swiften.svc@gmail.com>
2018-12-05 13:09:20 +0800

    test(redux-saga-effect): add tests for select effect

--- ReactiveReduxTests/ReduxSagaTest+Effect.swift
+++ ReactiveReduxTests/ReduxSagaTest+Effect.swift
@@ -63,6 +63,25 @@ final class ReduxSagaEffectTest: XCTestCase {
     XCTAssertEqual(dispatchCount, 1)
     [value1, value2, value3].forEach({XCTAssertEqual($0.value, 10)})
   }
+  
+  func test_selectEffect_shouldEmitOnlySelectedStateValue() {
+    /// Setup
+    var dispatchCount = 0
+    let dispatch: Redux.Store.Dispatch = {_ in dispatchCount += 1}
+    let effect = Redux.Saga.Effect<State, Int>.select({_ in 100})
+    let output = effect.invoke(withState: (), dispatch: dispatch)
+    
+    /// When
+    dispatch(Redux.Preset.Action.noop)
+    output.onAction(Redux.Preset.Action.noop)
+    let value1 = output.nextValue(timeoutInSeconds: 2)
+    let value2 = output.nextValue(timeoutInSeconds: 2)
+    let value3 = output.nextValue(timeoutInSeconds: 2)
+    
+    /// Then
+    XCTAssertEqual(dispatchCount, 1)
+    [value1, value2, value3].forEach({XCTAssertEqual($0.value, 100)})
+  }
 }
 
 extension ReduxSagaEffectTest {
