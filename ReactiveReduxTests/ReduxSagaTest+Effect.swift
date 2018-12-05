aed8fe7500de3a0ee3003afd9a4eb72d37045d1a
Hai Pham <swiften.svc@gmail.com>
2018-12-05 12:29:41 +0800

    test(redux-saga-effect): add tests for EmptyEffect

--- ReactiveReduxTests/ReduxSagaTest+Effect.swift
+++ ReactiveReduxTests/ReduxSagaTest+Effect.swift
@@ -20,14 +20,29 @@ final class ReduxSagaEffectTest: XCTestCase {
     /// When
     dispatch(Redux.Preset.Action.noop)
     output.onAction(Redux.Preset.Action.noop)
-    output.onAction(Redux.Preset.Action.noop)
-    output.onAction(Redux.Preset.Action.noop)
     let value = output.nextValue(timeoutInSeconds: 2)
     
     /// Then
     XCTAssertEqual(dispatchCount, 1)
     XCTAssert(value.error is Redux.Saga.Error)
     XCTAssertEqual(value.error as? Redux.Saga.Error, .unimplemented)
+  }
+  
+  func test_emptyEffect_shouldNotEmitAnything() {
+    /// Setup
+    var dispatchCount = 0
+    let dispatch: Redux.Store.Dispatch = {_ in dispatchCount += 1}
+    let effect: Redux.Saga.Effect<State, Int> = Redux.Saga.Effects.empty()
+    let output = effect.invoke(withState: (), dispatch: dispatch)
+    
+    /// When
+    dispatch(Redux.Preset.Action.noop)
+    output.onAction(Redux.Preset.Action.noop)
+    let value = output.nextValue(timeoutInSeconds: 2)
+    
+    /// Then
+    XCTAssertEqual(dispatchCount, 1)
+    XCTAssertTrue(value.isFailure)
   }
 }
 
